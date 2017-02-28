e490b1e2a1b4509797b865281ab3b8f2e60e8cad
Kyle Gundry <Gundr1kt@gmail.com>
2017-02-27 20:01:42 -0600

    Changed shoe 3 hash to not include price and add it in after

--- store_item.rb
+++ store_item.rb
@@ -1,6 +1,8 @@
 shoe1 = {"Brand" => "Nike", "Color" => "Black", "Price" => 99.99}
 shoe2 = {Brand: "Adidas", Color: "Silver", Price: 79.99}
-shoe3 = {:Brand => "Converse", :Color => "White", :Price => 59.99}
+shoe3 = {:Brand => "Converse", :Color => "White"}
+
+shoe3[:Price] = 59.99
 
 puts "I have " + shoe1["Color"] + " " + shoe1["Brand"] + " shoes that cost $" + shoe1["Price"].to_s + "."
 puts "I have #{shoe2[:Color]} #{shoe2[:Brand]} shoes that cost $#{shoe2[:Price]}."
