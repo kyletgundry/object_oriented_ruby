b4e4ccbd0b42a9ae508c34794ced45c09be4368a
Kyle Gundry <Gundr1kt@gmail.com>
2017-02-27 19:45:22 -0600

    Added 3 Shoes as hashes

---
+++ store_item.rb
@@ -0,0 +1,4 @@
+shoe1 = {"Brand" => "Nike", "Color" => "Black", "Price" => 99.99}
+shoe2 = {Brand: "Adidas", Color: "Silver", Price: 79.99}
+shoe3 = {:Brand => "Converse", :Color => "White", :Price => 59.99}
+
