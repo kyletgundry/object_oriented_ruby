10731398e12b9e3528f79cf6ec2d8f50cd6d8b60
Kyle Gundry <Gundr1kt@gmail.com>
2017-02-27 19:55:21 -0600

    Added put statements for 3 shoe hashes

--- store_item.rb
+++ store_item.rb
@@ -2,3 +2,7 @@ shoe1 = {"Brand" => "Nike", "Color" => "Black", "Price" => 99.99}
 shoe2 = {Brand: "Adidas", Color: "Silver", Price: 79.99}
 shoe3 = {:Brand => "Converse", :Color => "White", :Price => 59.99}
 
+puts "I have " + shoe1["Color"] + " " + shoe1["Brand"] + " shoes that cost $" + shoe1["Price"].to_s + "."
+puts "I have #{shoe2[:Color]} #{shoe2[:Brand]} shoes that cost $#{shoe2[:Price]}."
+puts "I have #{shoe3[:Color]} #{shoe3[:Brand]} shoes that cost $#{shoe3[:Price]}."
+
