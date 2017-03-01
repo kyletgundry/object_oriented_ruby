670c1c5573a8eb40e0889468e38398d29c58c37b
Kyle Gundry <Gundr1kt@gmail.com>
2017-02-28 19:21:19 -0600

    add 2 methods to fire all and increase all employees salary

--- manager.rb
+++ manager.rb
@@ -34,8 +34,26 @@ class Manager < Employee
     # use email sending library...
     puts "Email sent!"
   end
+
+  def give_all_raises
+    @employees.each do |employee|
+      employee.give_annual_raise
+    end
+  end
+
+  def fire_all_employees
+    @employees.each do |employee|
+      employee.active = false
+    end
+  end
 end
 
 manager = Manager.new(first_name: "Saron", last_name: "Yitbarek", salary: 100000, active: true, employees: [employee1, employee2])
 manager.print_info
 manager.send_report
+manager.give_all_raises
+
+puts employee1.print_info
+manager.fire_all_employees
+
+puts employee1.active
